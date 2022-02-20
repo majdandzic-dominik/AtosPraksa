3e429d75000c818a015e556971e6df6e72b4b2e6
Ubuntu <dominik.majdandzic@gmail.com>
2022-02-20 02:52:55 -0800

    Added 6th task

---
+++ CMake/zadatak6/build/CMakeFiles/zadatak6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadatak6.dir/src/main.cpp.o"
+  "zadatak6"
+  "zadatak6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zadatak6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
